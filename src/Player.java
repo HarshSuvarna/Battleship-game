f63e227a15d3c655e7723cc7ad9226590675f530
Harsh Suvarna <harsh.suvarna0@gmail.com>
2023-10-15 18:44:04 +0100

    functinalized few blocks of main method

--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class Player {
     private String name;
     private int score;
     private static String turn = "Player1";
+    private static Scanner scanner = new Scanner(System.in);
 
     public Player(String name) {
         this.name = name;
@@ -11,11 +14,11 @@ public class Player {
         return this.name;
     }
 
-    public void setTurn(String player) {
+    public static void setTurn(String player) {
         turn = player;
     }
 
-    public String getTurn() {
+    public static String getTurn() {
         return turn;
     }
 
@@ -25,5 +28,40 @@ public class Player {
 
     public int getScore() {
         return this.score;
+    }
+
+    public static int playerTurn(Player player, Square[][] board) {
+        try {
+            System.out.println(String.format("%s 's turn",
+                    player.getName()));
+            scanner.nextLine();
+            System.out.print("Enter row: ");
+            int row = scanner.nextInt();
+            System.out.print("Enter column: ");
+            int col = scanner.nextInt();
+            if (row < 0 || row > 9 || col < 0 || col > 9) {
+            return -1;
+            }
+            int outCome = board[row][col].playerGame(" X ", player);
+            return outCome;
+
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    public static boolean endgameLogic(Player player1, Player player2) {
+        int player1Score = player1.getScore();
+        int player2Score = player2.getScore();
+        if ((player1Score + player2Score) == 2) {
+            if (player1Score == player2Score) {
+                System.out.println("Game over. Its a tie!");
+            } else {
+                System.out.println(String.format("Game over. %s wins!! ",
+                        player1Score > player2Score ? player1.getName() : player2.getName()));
+            }
+            return false;
+        }
+        return true;
     }
 }
--- src/game.java
+++ src/game.java
@@ -4,46 +4,38 @@ public class game {
 	public static void main(String[] args) {
 		Board board = new Board(10, 10);
 		Square[][] newBoard = board.generateBoard();
-		for (int i = 0; i < 2; i++) {
+		for (int i = 0; i < 5; i++) {
 			Battleship battleship = new Battleship(newBoard);
 			battleship.generateBattleShip(battleship);
 		}
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter name of player 1: ");
+		String player1Name = s.nextLine();
+		Player player1 = new Player(player1Name);
+		System.out.print("Enter name of player 2");
+		String player2Name = s.nextLine();
+		Player player2 = new Player(player2Name);
+		boolean game = true;
+
+		System.out.println("Welcome to Battleship");
 		System.out.println(board.toString());
+		do {
+			String playerTurn = Player.getTurn();
 
-		Player player1 = new Player("Harsh");
-		Player player2 = new Player("Kurved");
-		try (Scanner scanner = new Scanner(System.in)) {
-			boolean game = true;
-			System.out.println("Welcome to Battleship");
-			do {
-				String playerTurn = player1.getTurn();
-				System.out.println(String.format("%s 's turn",
-						playerTurn == "Player1" ? player1.getName() : player2.getName()));
-				System.out.print("Enter row: ");
-				int row = scanner.nextInt();
-				System.out.print("Enter column: ");
-				int col = scanner.nextInt();
-				int outCome = newBoard[row][col].playerGame(" X ", playerTurn == "Player1" ? player1 : player2);
-				if (outCome > 0) {
-					player2.setTurn(playerTurn);
-				} else {
-					player2.setTurn(playerTurn == "Player1" ? "Player2" : "Player1");
-				}
-				System.out.println(board.toString());
-				int player1Score = player1.getScore();
-				int player2Score = player2.getScore();
-				if ((player1Score + player2Score) == 2) {
-					game = false;
-					if (player1Score == player2Score) {
-						System.out.println("Game over. Its a tie!");
-					} else {
-						System.out.println(String.format("Game over. %s wins!! ",
-								player1Score > player2Score ? player1.getName() : player2.getName()));
-					}
-				}
+			int outCome = Player.playerTurn(playerTurn.equals("Player1") ? player1 : player2, newBoard);
+			if (outCome > 0) {
+				Player.setTurn(playerTurn);
+			} else if (outCome == 0) {
+				Player.setTurn(playerTurn == "Player1" ? "Player2" : "Player1");
+			} else {
+				System.out.println("Please enter a valid input between 0 and 9");
+				Player.setTurn(playerTurn);
+			}
+			System.out.println(board.toString());
 
-			} while (game);
-		}
+			game = Player.endgameLogic(player1, player2);
 
+		} while (game);
+		s.close();
 	}
 }
